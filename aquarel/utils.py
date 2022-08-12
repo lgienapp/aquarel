2484c5fe4c7797ceeda8d14b4af60c98c2e3d81d
Lukas Gienapp <lukas.gienapp@uni-leipzig.de>
2022-08-13 00:33:15 +0200

    🔧 Fix #7

--- aquarel/utils.py
+++ aquarel/utils.py
@@ -43,17 +43,29 @@ def make_samples():
     """
     Generates sample plots for all themes to be used in documentation
     """
-    import numpy as np
+    import seaborn as sns
     import matplotlib.pyplot as plt
     for theme in list_themes():
         with load_theme(theme):
-            fig, ax = plt.subplots(1, 1)
-            x = np.linspace(0.0, 4.0)
-            plt.plot(x, np.cos(2 * np.pi * x) * np.exp(-x))
-            plt.plot(x, np.sin(2 * np.pi * x) * np.exp(-x))
-            plt.plot(x, np.sin(2 * np.pi * x) * np.log(x))
-            ax.set_title(theme)
-            ax.set_xlabel("X-Axis")
-            ax.set_ylabel("Y-Axis")
+            geysers = (
+                sns.load_dataset("geyser")
+                .rename(columns={"duration": "Duration", "kind": "Kind", "waiting": "Waiting"})
+                .replace({"long": "Long", "short": "Short"})
+            )
+            fig, ax = plt.subplots(1, 3, figsize=(15, 5))
+            # Hacky patched boxplot since seaborn overrides color options otherwise
+            # sns.boxplot(x="Kind", y="Duration", data=tips, ax=ax[0])
+            ax[0].boxplot(
+                [
+                    geysers.loc[geysers["Kind"] == "Long", "Duration"].tolist(),
+                    geysers.loc[geysers["Kind"] == "Short", "Duration"].tolist()
+                ],
+                vert=True,
+                patch_artist=True,
+                labels=geysers["Kind"].unique()
+            )
+            sns.kdeplot(x="Waiting", y="Duration", hue="Kind", data=geysers, ax=ax[1], fill=True)
+            sns.lineplot(x="Waiting", y="Duration", hue="Kind", data=geysers, ax=ax[2])
+            plt.suptitle("Geysers")
         fig.savefig(f"assets/{theme}.png", dpi=75, transparent=False, facecolor=fig.get_facecolor(),
                     bbox_inches='tight')
