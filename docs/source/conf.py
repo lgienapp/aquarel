31f143ba5f9626452f2d92d6aadd359d53812d6b
Lukas Gienapp <lukas.gienapp@uni-leipzig.de>
2022-08-31 14:14:56 +0200

    :bookmark: Release 0.0.4

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@ sys.path.insert(0, os.path.abspath('../../'))
 project = 'aquarel'
 copyright = '2022, Lukas Gienapp'
 author = 'Lukas Gienapp'
-release = '0.0.3'
+release = '0.0.4'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
